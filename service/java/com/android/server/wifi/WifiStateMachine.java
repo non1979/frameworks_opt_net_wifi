b7856e7e37b04efc8b4e03430e6e768912a1a906
Jessica Wagantall <jwagantall@cyngn.com>
2016-04-18 16:40:29 -0700

    Revert "WiFi not connecting to WiFi when screen OFF"
    Ticket: RM-234
    This reverts commit e26ad459b63271548abbdeba4f8d77fcca9f88bd.
    
    Change-Id: Ia2f700ee211c4bd54661fd1e1aa150761fff0d4a

--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -1151,7 +1151,7 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
         mP2pSupported = mContext.getPackageManager().hasSystemFeature(
                 PackageManager.FEATURE_WIFI_DIRECT);
 
-        mWifiNative = new WifiNative(mInterfaceName, mContext);
+        mWifiNative = new WifiNative(mInterfaceName);
         mWifiConfigStore = new WifiConfigStore(context,this,  mWifiNative);
         mWifiAutoJoinController = new WifiAutoJoinController(context, this,
                 mWifiConfigStore, mWifiConnectionStatistics, mWifiNative);
